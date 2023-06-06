9de4cf5258ddb81d3ea3e4a132888252cb12dd3b
Ryu-nakay <ryukenkana@gmail.com>
2023-06-07 08:37:07 +0900

    MeのModel作成

---
+++ RewriteEntranceExamWithSVVS/Model/Me.swift
@@ -0,0 +1,30 @@
+//
+//  Me.swift
+//  RewriteEntranceExamWithSVVS
+//
+//  Created by cw-ryu.nakayama on 2023/06/06.
+//
+
+import Foundation
+
+struct Me {
+    let accountId: Int
+    let roomId: Int
+    let name: String
+    let chatworkId: String
+    let organizationId: Int
+    let organizationName: String
+    let department: String
+    let title: String
+    let url: String
+    let introduction: String
+    let mail: String
+    let telOrganization: String
+    let telExtension: String
+    let telMobile: String
+    let skype: String
+    let facebook: String
+    let twitter: String
+    let avatarImageUrl: String
+    let loginMail: String
+}
