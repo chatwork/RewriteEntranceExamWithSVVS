c9eaa37a70f16400ada07c139b31460aa9eb16ae
Ryu-nakay <ryukenkana@gmail.com>
2023-06-07 10:44:52 +0900

    HTTPMethodの定義

---
+++ RewriteEntranceExamWithSVVS/Repository/Common/HTTPHethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPHethod.swift
+//  RewriteEntranceExamWithSVVS
+//
+//  Created by cw-ryu.nakayama on 2023/06/07.
+//
+
+import Foundation
+
+// リポジトリ共通で使うHTTPのメソッド(必要になったらメソッドは生やす)
+enum HTTPMethod: String {
+    case get = "GET"
+}
