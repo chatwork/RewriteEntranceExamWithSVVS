469f54025d5c3296345131420ef42987eb1f4d94
Ryu-nakay <ryukenkana@gmail.com>
2023-06-07 10:42:27 +0900

    MeRepository.fetchの型作成

---
+++ RewriteEntranceExamWithSVVS/Repository/MeRepository.swift
@@ -0,0 +1,15 @@
+//
+//  MeRepository.swift
+//  RewriteEntranceExamWithSVVS
+//
+//  Created by cw-ryu.nakayama on 2023/06/07.
+//
+
+import Foundation
+
+struct MeRepository {
+    // TODO: 引数の型変えたい
+//    func fetch(token: String) async throws -> Me {
+//
+//    }
+}
