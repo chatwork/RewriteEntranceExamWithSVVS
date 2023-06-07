500aa7b3e52e1763ebf3e76149acac2b18f732b0
Ryu-nakay <ryukenkana@gmail.com>
2023-06-07 14:52:09 +0900

    MeのDecodable準拠

--- RewriteEntranceExamWithSVVS/Model/Me.swift
+++ RewriteEntranceExamWithSVVS/Model/Me.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Me {
+struct Me: Decodable {
     let accountId: Int
     let roomId: Int
     let name: String
@@ -27,4 +27,26 @@ struct Me {
     let twitter: String
     let avatarImageUrl: String
     let loginMail: String
+    
+    enum CodingKeys: String, CodingKey {
+        case accountId = "account_id"
+        case roomId = "room_id"
+        case name = "name"
+        case chatworkId = "chatwork_id"
+        case organizationId = "organization_id"
+        case organizationName = "organization_name"
+        case department = "department"
+        case title = "title"
+        case url = "url"
+        case introduction = "introduction"
+        case mail = "mail"
+        case telOrganization = "tel_organization"
+        case telExtension = "tel_extension"
+        case telMobile = "tel_mobile"
+        case skype = "skype"
+        case facebook = "facebook"
+        case twitter = "twitter"
+        case avatarImageUrl = "avatar_image_url"
+        case loginMail = "login_mail"
+    }
 }
