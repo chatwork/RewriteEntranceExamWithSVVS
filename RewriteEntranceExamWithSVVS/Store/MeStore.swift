018dbb1baab67c42533c893dce34f55943263e7e
Ryu-nakay <ryukenkana@gmail.com>
2023-06-08 09:33:54 +0900

    MeStoreを実装し、テストのグリーンを確認

---
+++ RewriteEntranceExamWithSVVS/Store/MeStore.swift
@@ -0,0 +1,35 @@
+//
+//  MeStore.swift
+//  RewriteEntranceExamWithSVVS
+//
+//  Created by cw-ryu.nakayama on 2023/06/08.
+//
+
+import Foundation
+
+@MainActor
+final class MeStore {
+    static let shared: MeStore = .init()
+    
+    @Published private(set) var value: Me?
+    
+    private var meRepository: MeRepositoryProtocol = MeRepository()
+    
+    func fetch(token: String) async throws {
+        let fetchResult = try await meRepository.fetch(token: token)
+        value = fetchResult
+    }
+}
+
+// ユニットテスト用のメソッド
+extension MeStore {
+    // プロパティを初期状態にする
+    func setUpForUnitTest() {
+        value = nil
+        meRepository = MeRepository()
+    }
+    
+    func injectionMeRepositoryMock(mock: MeRepositoryProtocol) {
+        meRepository = mock
+    }
+}
--- RewriteEntranceExamWithSVVSTests/Store/MeStoreTests.swift
+++ RewriteEntranceExamWithSVVSTests/Store/MeStoreTests.swift
@@ -7,18 +7,19 @@
 
 import XCTest
 
+@MainActor
 final class MeStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Storeのvalueは private (set) になるはずなので、Store内にsetUp用のメソッドを置く(間違ってプロダクト内で呼ばれないかは心配)
-        let MeStore.shared.setUpForUnitTest()
+        MeStore.shared.setUpForUnitTest()
     }
     
     func testMeStoreからRepositoryを経由してMeの情報を取得できる() async throws {
-        // MeStoreはシングルトンで実装予定なのでどうやって一時的にRepositoryをMockに差し替えようか検討した結果、一時的にMockを渡してコールバックの中身を処理してもらう方法を考えた
-        MeStore.shared.temporaryReplaceMeRepository(MeRepositoryMock()) {
-            await MeStore.shared.fetch()
-            XCTAssertNotNil(MeStore.shared.value)
-        }
+        // MeStoreのRepositoryをMockに差し替え
+        MeStore.shared.injectionMeRepositoryMock(mock: MeRepositoryMock())
+        
+        try await MeStore.shared.fetch(token: "input your token")
+        XCTAssertNotNil(MeStore.shared.value)
     }
 }
