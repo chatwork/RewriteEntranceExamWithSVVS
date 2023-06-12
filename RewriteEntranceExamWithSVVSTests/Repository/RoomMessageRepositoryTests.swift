a29f262e741cda24f877ee48799a2aae530b472a
Ryu-nakay <ryukenkana@gmail.com>
2023-06-12 16:27:18 +0900

    RoomMessageRepositoryのテストを記述し、レッドを確認

---
+++ RewriteEntranceExamWithSVVSTests/Repository/RoomMessageRepositoryTests.swift
@@ -0,0 +1,20 @@
+//
+//  RoomMessageRepositoryTests.swift
+//  RewriteEntranceExamWithSVVSTests
+//
+//  Created by cw-ryu.nakayama on 2023/06/12.
+//
+
+import XCTest
+
+@MainActor
+final class RoomMessageRepositoryTests: XCTestCase {
+    let token = KeyManager().getValue(key: "ChatworkAPIToken") as! String
+    
+    func test_メッセージが送られ_message_idがStringで帰ってくる() async throws {
+        let repository = RoomMessageRepository()
+        let result = try await repository.put(token: ChatworkAPIToken(value: token), body: "テストメッセージ")
+        
+        XCTAssert(result is String)
+    }
+}
